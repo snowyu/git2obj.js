277ec263ed1bd4cb509cbc3cdc147810a8f49d9e
Riceball LEE <snowyu.lee@gmail.com>
2020-09-30 19:22:24 +0800

    chore: minor changes

--- src/git.ts
+++ src/git.ts
@@ -10,10 +10,12 @@ const defaultOpts = {
 async function walkTree(tree: TreeEntry[], options: any, objects: any[]) {
   await Promise.all(tree.map(async item => {
     const obj = await git.readObject(Object.assign({}, options, {oid: item.oid}))
+    delete obj.format
+    delete obj.source
+    objects.unshift(obj)
     if (item.type === 'tree') {
       return walkTree((obj as ParsedTreeObject).object, options, objects)
     }
-    objects.unshift(obj)
   }))
 }
 
@@ -33,10 +35,14 @@ export async function gitToObj(options: any = {}) {
   // let commit = await git.readObject(Object.assign({}, options, {oid: sha}))
   // objects.unshift({type: 'commit', oid: sha, object: commit})
   let obj: any = await git.readObject(Object.assign({}, options, {oid}))
+  delete obj.format
+  delete obj.source
   objects.unshift(obj)
 
   oid = (obj as ParsedCommitObject).object.tree
   obj = await git.readObject(Object.assign({}, options, {oid}))
+  delete obj.format
+  delete obj.source
   objects.unshift(obj)
   const tree = (obj as ParsedTreeObject).object
   await walkTree(tree, options, objects)
